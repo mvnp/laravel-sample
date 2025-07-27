87e91fe5416359adfbef68f78ebee1ccceb7e41a
Marcos Pereira <mvnpereira@gmail.com>
2025-07-27 18:45:09 +0000

    Api Construction

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Method 1: Full resource routes (recommended)
+Route::apiResource('estabelecimentos', EstabelecimentoController::class);
